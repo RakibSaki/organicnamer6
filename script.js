79c53e86fb69405f709b7e6e95bbed2324435db8
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-06-26 14:55:17 +0600

    Debug distance recording and Draw in html

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
-class Distance(distance, atom) {
+function Distance(distance, atom) {
     this.distance = distance
     this.atom = atom
 }
 
 function Atom(x, y, bondTo) {
     let distances = []
+    let bonds
+    this.addBond = () => bonds++
+    this.bondsFull = () => bonds == 4
+    this.recordDistance = (distance, atom) => {
+        distances.push(new Distance(distance, atom))
+    }
     if (!bondTo) {
         this.molecule = new Molecule(this)
+        bonds = 1
     } else {
+        bonds = 1
+        bondTo.addBond()
         this.molecule = bondTo.molecule
         this.molecule.add(this)
         // record new distance for this atom
-        distances.push(new Distance(1, bondTo))
-        bondTo.distances.push(new Distance(1, this))
+        this.recordDistance(1, bondTo)
         // record new distance for bonded atom
-        for (let distance of bondTo.distances) {
+        bondTo.recordDistance(1, this)
+        for (let distance of bondTo.peekDistances()) {
             // record new distance for this atom
-            distances.push(new Distance(distance.distance+1, distance.atom))
+            this.recordDistance(distance.distance+1, distance.atom)
             // record new distance for other atom
-            distance.atom.distances.push(new Distance(distance.distance+1, this))
+            distance.atom.recordDistance(distance.distance+1, this)
         }
     }
-    this.distanceTo(atom) {
+    this.distanceTo = (atom) => {
         for (let distance of distances) {
             if (distance.atom == atom) {
                 return distance.distance
             }
         }
+    }
+    this.peekDistances = () => distances
+    this.peekPosition = () => [x, y]
+    this.draw = () => {
+        let element = document.createElement('div')
+        element.classList.add('atom')
+        element.style.transform = `translate(${x}px, ${y}px)`
+        document.body.appendChild(element)
+        this.element = element
+        if (!bondTo) {
+            return
+        }
+        let bondLine = document.createElement('div')
+        bondLine.classList.add('bond')
+        let [bondToX, bondToY] = bondTo.peekPosition()
+        bondLine.style.left = `${(x + bondToX) / 2}px`
+        bondLine.style.top = `${(y + bondToY) / 2}px`
+        let width = Math.sqrt(((x - bondToX) * (x - bondToX)) + ((y - bondToY) * (y - bondToY)))
+        bondLine.style.width = `${width}px`
+        let rotate = Math.atan((bondToY - y) / (bondToX - x))
+        bondLine.style.transform = `translate(${width / -2}px, -1px) rotate(${rotate}rad)`
+        bondLine.style.zIndex = '-1'
+        document.body.appendChild(bondLine)
     }
 }
 
